59d4254017e34caad11dc1911a3ed92ab9e34ff5
Pornpichai <knkzuk@gmail.com>
2025-10-12 13:14:12 +0700

    NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
-   <h1 className='text-blue-500'>
-    HI
-   </h1>
-      
+   <div className="min-h-screen bg-gray-50 w-screen h-screen">
+      <NavBar />
+      <div className="p-8 text-center text-gray-700">
+        <h1 className="text-3xl font-bold">ยินดีต้อนรับสู่เว็บของคุณ!</h1>
+      </div>
+    </div>  
   )
 }
 
